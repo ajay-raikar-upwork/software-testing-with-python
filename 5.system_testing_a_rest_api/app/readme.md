13bb78e934621d5e42a2af58e6c63fc3df234535
ajayraikar-upwork <iit2017040@iiita.ac.in>
2020-05-11 23:06:26 +0530

    added files for acceptance testing.

--- 5.system_testing_a_rest_api/app/readme.md
+++ 5.system_testing_a_rest_api/app/readme.md
@@ -1,3 +1,5 @@
+[![Build Status](https://travis-ci.com/ajay-raikar-upwork/ci-testing-flask-rest-api.svg?branch=master)](https://travis-ci.com/ajay-raikar-upwork/ci-testing-flask-rest-api)
+
 # Stores REST Api
 
 This is built with Flask, Flask-RESTful, Flask-JWT, and Flask-SQLAlchemy.
---
+++ 6.acceptance_testing_and_browser_automation_with_selenium/app/tests/acceptance/steps/navigation.py
@@ -0,0 +1,26 @@
+from behave import *
+from selenium import webdriver
+
+use_step_matcher('re')
+
+
+@given('I am on the homepage')
+def step_impl(context):
+    context.driver = webdriver.Chrome('C:\\Users\\d33ps3curity\\Downloads\\chromedriver_win32\\chromedriver.exe')
+    context.driver.get('http://127.0.0.1:5000')
+
+@given('I am on the blog page')
+def step_impl(context):
+    context.driver = webdriver.Chrome('C:\\Users\\d33ps3curity\\Downloads\\chromedriver_win32\\chromedriver.exe')
+    context.driver.get('http://127.0.0.1:5000/blog')
+
+@then('I am on the blog page')
+def step_impl(context):
+    expected_url = 'http://127.0.0.1:5000/blog'
+    assert context.driver.current_url == expected_url
+
+
+@then('I am on the homepage')
+def step_impl(context):
+    expected_url = 'http://127.0.0.1:5000/'
+    assert context.driver.current_url == expected_url
